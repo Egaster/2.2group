66dfcaf3981b7ea6658259359489b9d3f32096d0
ilya_polukhin <71003158+JustAEro@users.noreply.github.com>
2023-02-28 22:01:43 +0300

    Update Lab1_3.java

--- lab1/lab1/lab1_3/src/Lab1_3.java
+++ lab1/lab1/lab1_3/src/Lab1_3.java
@@ -1,16 +1,43 @@
 import java.util.Scanner;
 
-public class Lab1_3 {
+public class Task3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a,b,c;
-        a = in.nextInt();
-        b = in.nextInt();
-        c = in.nextInt();
-        if(a>=b && a>=c) System.out.println(a);
-        if(b>=a && b>=c) System.out.println(b);
-        if(c>=b && c>=a) System.out.println(c);
-        System.out.println(Math.max(Math.max(a,b),Math.max(b,c)));
-    }
-}
 
+        System.out.print("Введите 1 целое число: ");
+        int a = in.nextInt();
+
+        System.out.print("Введите 2 целое число: ");
+        int b = in.nextInt();
+
+        System.out.print("Введите 3 целое число: ");
+        int c = in.nextInt();
+
+        //1 способ (условие)
+        int max1 = maxNoMath(a, b, c);
+        System.out.println("Результат 1 способа: " + max1);
+
+        //2 способ (Math.max())
+        int max2 = maxWithMath(a, b, c);
+        System.out.println("Результат 2 способа: " + max2);
+    }
+
+    private static int maxNoMath (int value1, int... rest){
+        int res = value1;
+        for (int value : rest){
+            if (value > res) {
+                res = value;
+            }
+        }
+        return res;
+    }
+
+    private static int maxWithMath(int value1, int... rest){
+        int res = value1;
+        for (int value : rest){
+            res = Math.max(res, value);
+        }
+        return res;
+    }
+
+}
